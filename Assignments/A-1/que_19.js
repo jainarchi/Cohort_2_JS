bf9f625ed20518f16f598815943afb61d6e19922
jainarchi <imarchijain2405@gmail.com>
2025-11-04 17:27:41 +0530

    q10

--- Assignments/A-1/que_19.js
+++ Assignments/A-1/que_19.js
@@ -1,32 +1,81 @@
 // let age = +prompt('enter your age');  // convert into number
 
+function validAge(age) {
+  if (age === null) {
+    console.log("you cancelled it");
+  } else if (age.trim().length === 0) {
+    console.log("give some input");
+  } else if (isNaN(age)) {
+    console.log("enter numbers only, without space ");
+  } else {
+    console.log(`your age is ${age}`);
+  }
+}
 
 let age = prompt('enter your age')
-console.log(age);
+validAge(age);
 
-if(age === null){
-    console.log('you cancelled it'); 
-}
+let password = (function () {
+  let p = null;
 
-else if( age.trim().length === 0){
-    console.log('give some input');
-}
+  function set() {
+    let pass = prompt("enter password to set");
+    if(pass !== null){
+        if (pass.trim().length > 3) {
+      p = pass.trim();
+      confirm("password set successfully");
+    } else {
+      alert("length must be greater than 3");
+    }
+    }
+    
+  }
 
-else if(isNaN(age)){
-  console.log('enter numbers only, without space ');    
-}
+  function show() {
+    if (p === null) {
+      console.log("not set yet");
+    } else console.log(p);
+  }
 
-else{
-    console.log(`your age is ${age}`);   
-}
+  function update() {
+    set();
+  }
 
+  function login() {
+    let open = confirm("Redirect to login.");
+   
+    if (open) {
+         if( p === null) {
+          console.log('Account not created yet');
+          return ;
+        }
 
-for(let i = 1 ; i <= 10 ; i++ ){
-    console.log(`5 * ${i} = ${5*i}`);
-}
+       let enterpass = prompt('enter password to login')
 
-let c = 0 ;
-for(let i = 1 ; i < 16 ; i++){
-    if(i > 8 ) c++;
-}
-console.log(c);
+       if(enterpass === null){
+        alert('Login cancelled')
+        return ;
+       }
+
+      if (enterpass === p) {
+        console.log("successfully loged in");
+      } else{
+        alert('Password is incorrect')
+      }  
+      } 
+    }
+  
+
+  return {
+    set,
+    show,
+    login,
+    update,
+  };
+})();
+
+password.set();
+// password.show()
+// password.update()
+// password.show()
+password.login();
