7e007ddc72b5822a24337966d4f7d4f27723026c
jainarchi <imarchijain2405@gmail.com>
2025-11-10 23:45:40 +0530

    level-1

---
+++ functionQue/level1.js
@@ -0,0 +1,83 @@
+
+
+function sayHey(){
+    console.log('hello javascript');
+    
+}
+
+sayHey();
+
+
+function add( a , b){
+    return a+ b ;
+}
+console.log(add(2 , 7));
+
+
+function greet(guest){
+    console.log(guest);
+}
+greet('hi Hina');
+
+
+// function addUnlimited(...nums){
+//     let sum =0 ;
+//     nums.forEach(element => {
+//          sum += element
+//     });
+//     console.log(sum);
+// }
+
+
+function addUnlimited(...nums){
+     
+ let ans = nums.reduce( (acc , val )=>{
+    return acc + val ;
+   } , 0)
+   console.log(ans);
+   
+
+}
+addUnlimited(1 , 2, 5 , 8, 7 , 8)
+
+
+// // create an IIFE that prints "I run instantly"
+// (function(){
+//      console.log('I run instantly');
+     
+// })();
+
+// closure -----------------------------
+// function parent(){
+//     let a = 90 ;
+
+//     function child (){
+//         console.log(a);
+        
+//     }
+
+//     child();
+// }
+
+// parent();
+
+
+
+let Obj ={
+    name : "archi",
+    age : 34 ,
+    city : "Bhopal"
+}
+
+for(let key in Obj){
+    console.log(Obj[key]);
+}
+
+
+
+
+
+
+
+
+
