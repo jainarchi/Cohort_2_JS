af31ff198dd112ec53d5a715db2b05b5593defef
jainarchi <imarchijain2405@gmail.com>
2025-11-21 00:20:37 +0530

    done task

--- Assignments/Like Animation/script.js
+++ Assignments/Like Animation/script.js
@@ -3,14 +3,22 @@ let img  = document.querySelector(".imgCont");
 
 img.addEventListener("dblclick" , ()=>{
    
-
     like.style.opacity = 1 ;
-
-    let st = setTimeout(function(){
-        like.style.opacity = 0 ;
-      
+    like.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)';
+    like.style.transition = 'cubic-bezier(1,1.84,.87,.82) 0.3s';
 
 
-    } , 900)
+    setTimeout(function(){
+        like.style.transform = 'translate(-50%, -250%) scale(1) rotate(40deg)';   
+
+    }, 900)
+
+    setTimeout(function(){
+         like.style.transform = 'translate(-50%, -50%) scale(0) rotate(-120deg)'
+         like.style.transition = "none"
+    } , 1200)
     
 })
+
+
+
