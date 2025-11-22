efb6d958e020fe530f4202dc0558b18fbf595ca2
jainarchi <imarchijain2405@gmail.com>
2025-11-22 17:39:09 +0530

    Build Insta features

---
+++ Assignments/Like Animation/insta.js
@@ -0,0 +1,214 @@
+const following = [
+
+  {
+    name: 'Amelia',
+    username: 'AmeliaJohnson10',
+    dp: "https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGdpcmx8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    name: 'Chloe',
+    username: 'ChloeUser15',
+    dp: "https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGdpcmx8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    name: 'Sofia',
+    username: 'SofiaGram22',
+    dp: "https://images.unsplash.com/photo-1602060306372-bdc3be71623a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D",
+    story: "https://images.unsplash.com/photo-1602233158242-3ba0ac4d2167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybHxlbnwwfHwwfHx8MA%3D%3D"
+  }
+  ,
+  {
+    name: 'Nova',
+    username: 'NovaStars31',
+    dp: "https://plus.unsplash.com/premium_photo-1661391295164-cdf5902bbd98?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8",
+    story: "https://images.unsplash.com/photo-1687539464665-f47e000779e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
+  }
+  ,
+  {
+    name: 'Evelyn',
+    username: 'EvelynRox88',
+    dp: "https://plus.unsplash.com/premium_photo-1661391295164-cdf5902bbd98?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8",
+    story: "https://images.unsplash.com/photo-1687539464665-f47e000779e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
+  }
+  ,
+  {
+    name: 'Maya',
+    username: 'MayaTheExplorer12',
+    dp: "https://plus.unsplash.com/premium_photo-1690205363056-94cf8f6ea542?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ,
+    story: "https://plus.unsplash.com/premium_photo-1711746523179-8b57eda9b813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+  },
+  {
+    name: 'Liam',
+    username: 'LiamTechGuru24',
+    dp: "https://plus.unsplash.com/premium_photo-1690205363056-94cf8f6ea542?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ,
+    story: "https://plus.unsplash.com/premium_photo-1711746523179-8b57eda9b813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+  },
+  {
+    name: 'Ethan',
+    username: 'EthanRider99',
+    dp: "https://plus.unsplash.com/premium_photo-1690205363056-94cf8f6ea542?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ,
+    story: "https://plus.unsplash.com/premium_photo-1711746523179-8b57eda9b813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+  }
+]
+const posts = [
+    {
+        dp: "https://images.unsplash.com/photo-1540189549336-e04743e498d9?w=600",
+        username: "FoodieFiesta_91",
+        post_img: "https://images.unsplash.com/photo-1540189549336-e04743e498d9?w=800",
+        time: "20min" 
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600",
+        username: "TravelerKate_14",
+        post_img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=800",
+        time: "3hr"
+    },
+    {
+        dp: "https://plus.unsplash.com/premium_photo-1661391295164-cdf5902bbd98?w=600",
+        username: "FitnessFreak_55",
+        post_img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b23f4?w=800",
+        time: "15hr"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1506794778202-dfa929cf3205?w=600",
+        username: "CoderGeek_88",
+        post_img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800",
+        time: "2d"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1675663359918-79997e76a357?w=600",
+        username: "AbstractArt_27",
+        post_img: "https://images.unsplash.com/photo-1579546929518-9e396f0ab877?w=800",
+        time: "5d" 
+    }
+];
+
+let stories = document.getElementById("stories")
+let showStory = document.getElementById("showStory")
+let showStoryDp = document.getElementById("showStory-dp")
+let showStoryUsername = document.getElementById("showStory-username")
+let showhr = document.getElementById("hrAgo")
+let feed = document.getElementById("feed")
+
+
+
+function renderStories() {
+
+  following.forEach(function (elem, idx) {
+
+    let box = document.createElement("div");
+    box.classList.add("box")
+
+    let story = document.createElement("div");
+    story.classList.add("story");
+    story.style.backgroundImage = `url(${elem.dp})`
+    story.id = idx;
+
+
+    let name = document.createElement("h6");
+    name.classList.add("name")
+    name.textContent = `${elem.name}`;
+
+    box.appendChild(story)
+    box.appendChild(name)
+    stories.appendChild(box);
+
+  })
+}
+
+function showStoriesOnClick() {
+
+  stories.addEventListener("click", (dets) => {
+    let i = dets.target.id
+    showStory.style.backgroundImage = `url(${following[i].story})`
+
+    showStoryDp.style.backgroundImage = `url(${following[i].dp})`
+    showStoryUsername.textContent = `${following[i].username}`;
+    showhr.textContent = `${Math.floor(Math.random() * 25)}h`
+
+
+    showStory.style.display = "block"
+
+    setTimeout(function () {
+      showStory.style.display = "none"
+    }, 1200)
+
+
+  })
+}
+
+
+function showFeed (){
+
+posts.forEach(function(p){
+
+
+  let post = document.createElement("div")
+  post.classList.add("post")
+
+
+   let user = document.createElement("div")
+   user.innerHTML = `<div class="post-dtl">
+                <div class="post-dp"><img src="${p.dp}"></div>
+                <span class="post-username">${p.username} </span>
+                <span class="dayAgo">${p.time}</span>
+            </div>`
+ 
+
+    let postImg = document.createElement("div")
+    postImg.classList.add('postImg')
+    postImg.style.backgroundImage = `url(${p.post_img})`;
+
+    let feedback = document.createElement("div")
+    feedback.classList.add("feedback")
+    feedback.innerHTML = ` <div class="f1">
+                        <i class="ri-heart-3-line"></i> 
+                        <i class="ri-chat-1-line"></i>
+                        <i class="ri-send-plane-line"></i>
+                    </div> 
+                    <i class="ri-bookmark-line"></i>`
+
+   post.appendChild(user)
+   post.appendChild(postImg)
+   post.appendChild(feedback)
+   feed.appendChild(post);
+
+   })
+}
+
+
+
+
+
+
+renderStories()
+showStoriesOnClick()
+
+showFeed()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add Event Listener on Parent instead each child 
