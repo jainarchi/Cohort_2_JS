12002e0b42f966f3b657305d916a412651c52bca
jainarchi <imarchijain2405@gmail.com>
2025-11-23 00:56:19 +0530

    done

--- Assignments/Insta Features/insta.js
+++ Assignments/Insta Features/insta.js
@@ -3,14 +3,22 @@ const following = [
   {
     name: 'Amelia',
     username: 'AmeliaJohnson10',
-    dp: "https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGdpcmx8ZW58MHx8MHx8fDA%3D"
+    story: "https://images.unsplash.com/photo-1748154228828-37f5599ff302?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    dp: "https://media.istockphoto.com/id/1401950372/photo/portrait-of-a-happy-young-woman-on-a-background-of-beautiful-sea.jpg?s=612x612&w=0&k=20&c=iJLiTUpTJq3XpSx7oDXA87QEnuNCKwtFvHaDS9wQj1k="
   },
+   {
+    name: 'Liam',
+    username: 'LiamTechGuru24',
+    dp: "https://images.unsplash.com/photo-1571764303004-255d3fdc5a6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"
+    ,
+    story: "https://images.unsplash.com/photo-1616236800965-0afe257d60cd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  }
+  ,
   {
-    name: 'Chloe',
-    username: 'ChloeUser15',
-    dp: "https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGdpcmx8ZW58MHx8MHx8fDA%3D"
+    name: 'Nova',
+    username: 'NovaStars31',
+    dp: "https://plus.unsplash.com/premium_photo-1688598498264-51323f3c9a35?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI4fHx8ZW58MHx8fHx8",
+    story: "https://images.unsplash.com/photo-1514315384763-ba401779410f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGdpcmx8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: 'Sofia',
@@ -20,73 +28,56 @@ const following = [
   }
   ,
   {
-    name: 'Nova',
-    username: 'NovaStars31',
-    dp: "https://plus.unsplash.com/premium_photo-1661391295164-cdf5902bbd98?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8",
-    story: "https://images.unsplash.com/photo-1687539464665-f47e000779e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
-  }
-  ,
-  {
-    name: 'Evelyn',
-    username: 'EvelynRox88',
-    dp: "https://plus.unsplash.com/premium_photo-1661391295164-cdf5902bbd98?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8",
-    story: "https://images.unsplash.com/photo-1687539464665-f47e000779e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
-  }
-  ,
-  {
     name: 'Maya',
     username: 'MayaTheExplorer12',
     dp: "https://plus.unsplash.com/premium_photo-1690205363056-94cf8f6ea542?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ,
-    story: "https://plus.unsplash.com/premium_photo-1711746523179-8b57eda9b813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+    story: "https://images.unsplash.com/photo-1674983820758-1ca937141978?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
   },
-  {
-    name: 'Liam',
-    username: 'LiamTechGuru24',
-    dp: "https://plus.unsplash.com/premium_photo-1690205363056-94cf8f6ea542?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    ,
-    story: "https://plus.unsplash.com/premium_photo-1711746523179-8b57eda9b813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
-  },
+ 
   {
     name: 'Ethan',
     username: 'EthanRider99',
-    dp: "https://plus.unsplash.com/premium_photo-1690205363056-94cf8f6ea542?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    dp: "https://images.unsplash.com/photo-1551022372-0bdac482b9d6?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ,
-    story: "https://plus.unsplash.com/premium_photo-1711746523179-8b57eda9b813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+    story: "https://images.unsplash.com/photo-1502307100811-6bdc0981a85b?q=80&w=678&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ]
 const posts = [
-    {
-        dp: "https://images.unsplash.com/photo-1540189549336-e04743e498d9?w=600",
-        username: "FoodieFiesta_91",
-        post_img: "https://images.unsplash.com/photo-1540189549336-e04743e498d9?w=800",
-        time: "20min" 
+   {
+        post_img: "https://images.unsplash.com/photo-1561347981-969c80cf4463?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29kZXJzJTIwcXVpdHN8ZW58MHx8MHx8fDA%3D",
+        username: "CoderGeek_88",
+        dp: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800",
+        time: "2d"
     },
     {
-        dp: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600",
+        post_img: "https://media.istockphoto.com/id/1092368518/photo/tourist-female-traveling-in-iceland-stands-near-magnificent-waterfall.jpg?s=612x612&w=0&k=20&c=cdAotedlbHv2EG7cBb1CTyshUgZ48mpuJqlbwoPo6vQ=",
         username: "TravelerKate_14",
-        post_img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=800",
+        dp: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=800",
         time: "3hr"
     },
     {
         dp: "https://plus.unsplash.com/premium_photo-1661391295164-cdf5902bbd98?w=600",
         username: "FitnessFreak_55",
-        post_img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b23f4?w=800",
+        post_img: "https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zml0bmVzc3xlbnwwfHwwfHx8MA%3D%3D",
         time: "15hr"
     },
-    {
-        dp: "https://images.unsplash.com/photo-1506794778202-dfa929cf3205?w=600",
-        username: "CoderGeek_88",
-        post_img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800",
-        time: "2d"
+     {
+        dp: "https://images.unsplash.com/photo-1564931768730-7e4d8e240044?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kZXJzJTIwcXVpdHN8ZW58MHx8MHx8fDA%3D",
+        username: "FoodieFiesta_91",
+        post_img: "https://images.unsplash.com/photo-1549921174-b16174c03ee2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2RpZXxlbnwwfHwwfHx8MA%3D%3D",
+        time: "20min" 
     },
+   
     {
         dp: "https://images.unsplash.com/photo-1675663359918-79997e76a357?w=600",
         username: "AbstractArt_27",
-        post_img: "https://images.unsplash.com/photo-1579546929518-9e396f0ab877?w=800",
+        post_img: "https://media.istockphoto.com/id/1162649037/vector/palestinian-design-element-14.jpg?s=612x612&w=0&k=20&c=f94IKyuqHfA8PKpr9C4Yl4p4LiNHkJ4T9xy3TKc9tSY=",
         time: "5d" 
     }
 ];
+
+
 
 let stories = document.getElementById("stories")
 let showStory = document.getElementById("showStory")
@@ -129,7 +120,7 @@ function showStoriesOnClick() {
 
     showStoryDp.style.backgroundImage = `url(${following[i].dp})`
     showStoryUsername.textContent = `${following[i].username}`;
-    showhr.textContent = `${Math.floor(Math.random() * 25)}h`
+    showhr.textContent = `${Math.floor(Math.random() * 25)+1}h`
 
 
     showStory.style.display = "block"
@@ -142,15 +133,12 @@ function showStoriesOnClick() {
   })
 }
 
-
 function showFeed (){
 
-posts.forEach(function(p){
-
+posts.forEach(function(p , idx){
 
   let post = document.createElement("div")
   post.classList.add("post")
-
 
    let user = document.createElement("div")
    user.innerHTML = `<div class="post-dtl">
@@ -164,15 +152,20 @@ posts.forEach(function(p){
     postImg.classList.add('postImg')
     postImg.style.backgroundImage = `url(${p.post_img})`;
 
+    postImg.innerHTML = `<i id=${idx} class="ri-heart-3-fill like-icon"></i>`
+    postImg.id = `${idx}`
+
+
+
     let feedback = document.createElement("div")
     feedback.classList.add("feedback")
     feedback.innerHTML = ` <div class="f1">
-                        <i class="ri-heart-3-line"></i> 
+                        <i class="ri-heart-3-line , likeCountBtn"></i> 
                         <i class="ri-chat-1-line"></i>
                         <i class="ri-send-plane-line"></i>
                     </div> 
                     <i class="ri-bookmark-line"></i>`
-
+   
    post.appendChild(user)
    post.appendChild(postImg)
    post.appendChild(feedback)
@@ -181,6 +174,46 @@ posts.forEach(function(p){
    })
 }
 
+function likeFunction(){
+    let posts = document.querySelectorAll(".postImg")
+    let likeIcons = document.querySelectorAll(".like-icon") 
+    let likeCountBtns = document.querySelectorAll(".likeCountBtn") 
+
+   posts.forEach((post) =>{
+     post.addEventListener("dblclick" , (e)=>{
+
+      let like = likeIcons[e.target.id]
+      
+      like.style.opacity = 1 ;
+      like.style.transform = `translate(-50% , -50%) rotate(0deg) scale(1)`
+      
+      setTimeout(function(){
+        like.style.transform = `translate(-50% , -250%) rotate(60deg) scale(1)`
+
+      } , 1000)
+
+      setTimeout(function(){
+         like.style.opacity = 0 ;
+         like.style.transform = `translate(-50% , -50%) rotate(-90deg) scale(0)`
+         like.style.transition = 'none'
+         
+      },1200)
+    
+      let btn =  likeCountBtns[e.target.id] ;
+      btn.style.backgroundColor = "red";
+      btn.style.backgroundClip = "text";
+      btn.style.color = "transparent";
+
+      btn.classList.remove('ri-heart-3-line');
+      btn.classList.add('ri-heart-3-fill');
+     
+     })
+   })
+    
+    
+}
+
+
 
 renderStories()
 showStoriesOnClick()
@@ -188,7 +221,7 @@ showStoriesOnClick()
 showFeed()
 
 
-
+likeFunction()
 
 
 
